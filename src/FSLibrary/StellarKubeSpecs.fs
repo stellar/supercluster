0a7b8f4a98a57503f19f0f54cc6a4eb811158fed
marta-lokhova <marta@stellar.org>
2020-01-30 08:47:53 -0800

    Reduce container readiness delays

--- src/FSLibrary/StellarKubeSpecs.fs
+++ src/FSLibrary/StellarKubeSpecs.fs
@@ -106,9 +106,9 @@ let CoreContainerForCommand (q:NetworkQuotas) (numContainers:int) (configOpt:Con
 
 let WithReadinessProbe (container:V1Container) probeTimeout : V1Container =
     let httpPortStr = IntstrIntOrString(value = CfgVal.httpPort.ToString())
-    let readyProbe = V1Probe(periodSeconds = System.Nullable<int>(5),
-                             initialDelaySeconds = System.Nullable<int>(10),
-                             failureThreshold = System.Nullable<int>(6),
+    let readyProbe = V1Probe(periodSeconds = System.Nullable<int>(1),
+                             initialDelaySeconds = System.Nullable<int>(1),
+                             failureThreshold = System.Nullable<int>(60),
                              timeoutSeconds = System.Nullable<int>(probeTimeout),
                              httpGet = V1HTTPGetAction(path = "/info",
                                                        port = httpPortStr))
