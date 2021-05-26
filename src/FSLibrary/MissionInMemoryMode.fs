6630f07aab7cb38bf71a3bafc8aea010c8604097
marta-lokhova <marta@stellar.org>
2021-05-26 16:09:30 -0700

    Add a new mission to test in-memory mode

---
+++ src/FSLibrary/MissionInMemoryMode.fs
@@ -0,0 +1,34 @@
+// Copyright 2021 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+module MissionInMemoryMode
+
+open StellarCoreSet
+open StellarMissionContext
+open StellarFormation
+open StellarSupercluster
+open StellarCoreHTTP
+
+let runInMemoryMode (context : MissionContext) =
+    let coreSet = MakeLiveCoreSet "core" { CoreSetOptions.GetDefault context.image with dumpDatabase = false }
+
+    let coreSetWithCaptiveCore = MakeLiveCoreSet
+                                  "in-memory-mode"
+                                  { CoreSetOptions.GetDefault context.image with
+                                      nodeCount = 1;
+                                      inMemoryMode = true;
+                                      validate = false;
+                                      localHistory = false;
+                                      quorumSet = CoreSetQuorum(CoreSetName "core") }
+
+    let context = { context with numAccounts = 100; numTxs = 100; txRate = 20 }
+
+    context.Execute [coreSet; coreSetWithCaptiveCore] None (fun (formation: StellarFormation) ->
+        formation.WaitUntilSynced [coreSet; coreSetWithCaptiveCore]
+        formation.UpgradeProtocolToLatest [coreSet; coreSetWithCaptiveCore]
+        formation.UpgradeMaxTxSize [coreSet; coreSetWithCaptiveCore] 100000
+
+        formation.RunLoadgen coreSet context.GenerateAccountCreationLoad
+        formation.RunLoadgen coreSet context.GeneratePaymentLoad
+    )
